7537fec725821eec79322db6352f004dfacdee5b
jette <jette.heinsohn@haw-hamburg.de>
2018-11-17 17:26:44 +0100

    Umbau

--- Sinus.py
+++ Sinus.py
@@ -6,16 +6,15 @@ fs = 44100       # sampling rate, Hz, must be integer
 duration = 0.01   # in seconds, may be float
 f = 440.0        # sine frequency, Hz, may be float
 # generate samples, note conversion to float32 array
-while True:
-    samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32) # for paFloat32 sample values must be in range [-1.0, 1.0]
-    stream = p.open(format=pyaudio.paFloat32,
-                   channels=1,
-                   rate=fs,
-                   output=True)
-    # play. May repeat with different volume values (if done interactively)
-    stream.write(volume*samples)
-    '''stream.stop_stream()
-    stream.close()
-    p.terminate()
-    '''
+samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32) # for paFloat32 sample values must be in range [-1.0, 1.0]
+stream = p.open(format=pyaudio.paFloat32,
+                channels=1,
+                rate=fs,
+                output=True)
+# play. May repeat with different volume values (if done interactively)
+stream.write(volume*samples)
+stream.stop_stream()
+stream.close()
+p.terminate()
+
 
