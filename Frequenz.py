040dfda8bf00b1475106ccdcaa1ff85730a823fc
jette <jette.heinsohn@haw-hamburg.de>
2018-11-17 17:18:28 +0100

    Umbau

--- Frequenz.py
+++ Frequenz.py
@@ -4,12 +4,10 @@ LowTon = 28 #c
 
 Distanz = float(input("Entfernung: "))
 print(Distanz)
-n = int(-float((HighTon-LowTon)/MAX)*Distanz+HighTon) #höchster Ton unten
-#n = int(((HighTon-LowTon)/MAX)*Distanz+LowTon) #tiefster Ton unten
+#n = int(-float((HighTon-LowTon)/MAX)*Distanz+HighTon) #höchster Ton unten
+n = int(float((HighTon-LowTon)/MAX)*Distanz+LowTon) #tiefster Ton unten
 print(n)
-Zwischen = float(n-49)
-print(Zwischen)
-Frequenz = round(2**(Zwischen/12)*440,3)
+Frequenz = round(2**((n-49)/12)*440,3)
 print(Frequenz)
 
 
--- Sinus.py
+++ Sinus.py
@@ -3,7 +3,7 @@ import numpy as np
 p = pyaudio.PyAudio()
 volume = 0.5     # range [0.0, 1.0]
 fs = 44100       # sampling rate, Hz, must be integer
-duration = 1.0   # in seconds, may be float
+duration = 5.0   # in seconds, may be float
 f = 440.0        # sine frequency, Hz, may be float
 # generate samples, note conversion to float32 array
 samples = (np.sin(2*np.pi*np.arange(fs*duration)*f/fs)).astype(np.float32)
