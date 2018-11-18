0a91326bd9bd6de7e4f8d52a3c8b4a37f67ef9e0
jette <jette.heinsohn@haw-hamburg.de>
2018-11-18 14:54:18 +0100

    Es Gibt Sound!

--- PureDataVariableFrequenz.py
+++ PureDataVariableFrequenz.py
@@ -1,6 +1,11 @@
-import os
+import socket
 Frequenz = input("Entfernung")
-def changeFrequenzy(f):
-    os.system("echo '"+ Frequenz + "' pdsend 3000")
+s = socket.socket()
+host = socket.gethostname()
+port = 3000
+s.connect((host, port))
+message = Frequenz + " ;" #Need to add " ;" at the end so pd knows when you're finished writing.
+s.send(message.encode('utf-8'))
 
-#df
+
+
