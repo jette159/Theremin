c3cf6e71e1c8fe5a95d5997241ccca98ff30f5fe
jette159 <Saustall1!>
2018-12-18 11:19:58 +0100

    switch

--- AddingSwitch.py
+++ AddingSwitch.py
@@ -292,8 +292,9 @@ def showTonindex(text):
     Index = str(text)
     with canvas(device) as draw:
 
-        draw.rectangle1(device.bounding_box, fill="black", outline="white")
-        draw.rectangle2(40,40, fill="White", outline="white")
+        draw.rectangle(device.bounding_box, fill="black", outline="white")
+        #draw.rectangle(40,40, fill="White", outline="white")
+        draw.text((10,0), text= "Mode"+ Mode, fill="white", font=font)
         draw.text((40,15 ), text= TonindexToBuchstabe[Index], fill= "white", font=font)
 
 def get_mode ():
