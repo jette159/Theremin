1f2cbc8350ef91d65b975b6f912912ae5cb39952
jette159 <Saustall1!>
2018-12-18 10:03:34 +0100

    switch

--- SwitchAuslesen.py
+++ SwitchAuslesen.py
@@ -9,7 +9,7 @@ GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 while True:
     input_state1 = GPIO.input(38)
     input_state2 = GPIO.input(36)
-    if input_state1 == False and input_state2 === True:
+    if input_state1 == False and input_state2 == True:
         print ("1")
     elif input_state1 == True and input_state2 == False:
         print ("2")
