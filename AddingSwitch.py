7775db99fec36aa4d96449ccda1700eafececd9e
Jette <jette.h.97@web.de>
2019-01-08 11:21:37 +0100

    Theorethisch funktioniert das jetzt also das es mehr sample werte gibt mit variabler anzahl

--- AddingSwitch.py
+++ AddingSwitch.py
@@ -18,11 +18,11 @@ GPIO_TRIGGER_V = 29
 GPIO_ECHO_V = 31
 
 #LED strip Case configuration:
-LED_COUNT      = 9              # Number of LED pixels.
+LED_COUNT      = 26             # Number of LED pixels.
 LED_PIN        = 18             # GPIO pin connected to the pixels (18 uses PWM!).
 LED_FREQ_HZ    = 800000         # LED signal frequency in hertz (usually 800khz)
 LED_DMA        = 10             # DMA channel to use for generating signal (try 10)         Was macht das?
-LED_BRIGHTNESS = 40             # Set to 0 for darkest and 255 for brightest
+LED_BRIGHTNESS = 55            # Set to 0 for darkest and 255 for brightest
 LED_INVERT     = False          # True to invert the signal (when using NPN transistor level shift)
 LED_CHANNEL    = 0              # set to '1' for GPIOs 13, 19, 41, 45 or 53
 
